4af033fa1e8efbfcf7db9477cda1b94485824266
SouhilRM <r.m.souhil47@gmail.com>
2023-04-04 23:42:12 +0100

    BDD part 04: flash message

--- resources/js/Pages/Layouts/MainLayout.vue
+++ resources/js/Pages/Layouts/MainLayout.vue
@@ -4,6 +4,10 @@
     <Link href="/show">SHOW</Link>&nbsp;
     <Link href="/index">indexlisting</Link>&nbsp;
     <Link href="/create">Create Listing</Link>
+
+    <div v-if="flashSuccess" class="success">
+        {{ flashSuccess }}
+    </div>    
     
     <h2>{{ timer }}</h2>
 
@@ -18,13 +22,19 @@
 </template>
 
 <script setup>
-    import { Link } from '@inertiajs/vue3'
+    import { Link, usePage } from '@inertiajs/vue3'
+    import { computed } from 'vue'
 
-    //"ref" permet de declarer un truc en temps reel ideal pour le temps
+    //"ref" permet de declarer un truc en temps reel (variable, nombre, chaine de cra, PAS UN OBJET) ideal pour le temps
     import { ref } from "vue";
     const timer = ref(0)
-
     setInterval(()=> timer.value++, 1000)
+
+    //pour itiliser les sharing data t'as besoin du 'usePage' noublie pas de l'ipmorter 
+    //on a aussi importer le 'computed' depuis vue juste pour ne pas ecrire une grosse expression à chaque fois que t'as une expression enorme avec de la logique utilise 'computed' comme vue et declare ta logique dans une fonction que tu pourras utiliser plus tard.
+    const flashSuccess = computed(
+        () => usePage().props.flash.success,
+    )
 </script>
 
 <style scoped>
@@ -37,5 +47,9 @@
         border-radius: 12px;
         box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
         padding: 1rem;
+    }
+    .success {
+        background-color: green;
+        color: white;
     }
 </style>
